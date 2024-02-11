798b6e876166b324f542337ece9a9240c15beb8f
Rki0 <pky00823@gmail.com>
2024-02-11 16:06:18 +0900

    better case of instance usage

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -11,27 +11,21 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messagesServie: MessagesService;
-
-  constructor() {
-    // DON'T DO THIS ON REAL APPS
-    // USE DEPENDENCY INJECTION
-    this.messagesServie = new MessagesService();
-  }
+  constructor(public messagesService: MessagesService) {}
 
   @Get()
   listMessages() {
-    return this.messagesServie.findAll();
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    return this.messagesServie.create(body.content);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   async getMessage(@Param('id') id: string) {
-    const message = await this.messagesServie.findOne(id);
+    const message = await this.messagesService.findOne(id);
 
     if (!message) {
       throw new NotFoundException('message not found');
