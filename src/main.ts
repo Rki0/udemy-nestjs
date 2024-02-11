382f07bdd39fe5e8fb4c3efd37cfaa73a61b96bc
Rki0 <pky00823@gmail.com>
2024-02-11 14:38:51 +0900

    bad case of instances usage

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+
+  // set global pipe
+  // pipe === input validator => using Data Transfer Object(DTO)
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,15 +1,42 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-messages.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesServie: MessagesService;
+
+  constructor() {
+    // DON'T DO THIS ON REAL APPS
+    // USE DEPENDENCY INJECTION
+    this.messagesServie = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-    return 'hello message';
+    return this.messagesServie.findAll();
   }
 
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesServie.create(body.content);
+  }
 
   @Get('/:id')
-  getMessage() {}
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesServie.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
+  }
 }
